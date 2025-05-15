53ffed70e2d177cdc73955d5efe5f7df37b30f62
Hrnjez <54723911+Hrnjez@users.noreply.github.com>
2025-05-16 00:17:06 +0200

    Update coins.js

--- api/coins.js
+++ api/coins.js
@@ -1,9 +1,22 @@
 const axios = require('axios');
 
+let cache = {
+  timestamp: 0,
+  data: null
+};
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://www.counterparty.tv');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const isCron = req.headers['x-vercel-cron'];
+  const now = Date.now();
+  const isCacheFresh = now - cache.timestamp < 60 * 60 * 1000; // 1 hour
+
+  if (!isCron && isCacheFresh && cache.data) {
+    return res.status(200).json(cache.data); // serve from cache
+  }
 
   try {
     const response = await axios.get(
@@ -27,6 +40,12 @@ module.exports = async (req, res) => {
       price: coin.quote.USD.price,
       percent_change_24h: coin.quote.USD.percent_change_24h,
     }));
+
+    // Store to cache
+    cache = {
+      timestamp: now,
+      data
+    };
 
     res.status(200).json(data);
   } catch (error) {
